3766dcd4dd2fc0f149882343bf8e0716c1459d92
junho-baek <86907815+junho-baek@users.noreply.github.com>
2023-08-15 05:19:15 +0000

    #5.1 Tour of CRA (12:40)
    Create React App을 사용하면 css도 모듈화가 가능하다. import 해서 style을 붙여넣을 때 이전에는 class와 id 등을 붙여두고 해당하는 스타일을 만들어줬다면, 모듈화된 css를 사용하면 css 문법을 이용해 만든 것을 컴포넌트에 주입할 수 있으므로 클래스네임을 일일히 다 기억할 필요가 없다. 실제로 html 을 보면 style모듈로 준 클래스네임은 랜덤하게 생성되는 것을 확인할 수 있다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import './App.css'
+import './App.css';
+import Button from './Button';
+import styles from './App.module.css';
+
+
 
 export default function App() {
   return (
     <main>
-     <h3>it is me!!!!!!!!!!</h3>
+     <h3 className = {styles.title}>it is me!!!!!!!!!!</h3>
+     <Button text={"hi"}/>
     </main>
   )
 }
+
+
